4d690f23941311762b366c89690c18d1a2965e35
flex021 <congthong2015@gmail.com>
2025-09-09 20:41:59 +0700

    fix vite

--- react-web/vite.config.js
+++ react-web/vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-
 export default defineConfig({
   define: {
     'process.env': process.env
   },
   plugins: [
     react(),
-    svgr()
+    svgr({
+      svgrOptions: {
+        icon: true
+      }
+    })
   ],
   // base: './'
   resolve: {
